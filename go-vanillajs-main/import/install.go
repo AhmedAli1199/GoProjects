45d954524a5f9ec81a5c82003343b01ab853f73b
Ahmed Ali <i220825@nu.edu.pk>
2025-07-01 15:34:44 +0500

    Database init and configured

--- go-vanillajs-main/import/install.go
+++ go-vanillajs-main/import/install.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	// Database connection string for your remote PostgreSQL database
-	connStr := "postgres://username:password@remote-host:port/database?sslmode=disable"
+	connStr := "***REMOVED***"
 
 	// Open database connection
 	db, err := sql.Open("postgres", connStr)
